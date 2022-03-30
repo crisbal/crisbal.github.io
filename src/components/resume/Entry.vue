a7c14f0bbc09d56a1185daf7eaa38e2811934ba7
Cristian Baldi <bld.cris.96@gmail.com>
2022-03-30 14:18:06 +0200

    WIP

--- src/components/resume/Entry.vue
+++ src/components/resume/Entry.vue
@@ -1,10 +1,11 @@
 <template lang="pug">
 .Entry
   .Entry__meta
-    .Entry__place {{ place }}
+    .Entry__title {{ title }}
+    .Entry__place &nbsp;- {{ place }}
+    .spacer
     .Entry__date {{ date }}
   .Entry__content
-    .Entry__title {{ title }}
     .Entry__details
       ul
         li(v-for="detail in details") {{ detail }}
@@ -21,15 +22,21 @@ export default {
 @import "@/style/mixins.scss";
 
 .Entry {
-  display: flex;
-
+  display: block;
   @include respond-below(sm) {
     display: block;
   }
 
   &__meta {
-    flex-basis: 22%;
-    padding-right: 0.75rem;
+    display: flex;
+    align-items: flex-end;
+
+    .spacer {
+      flex: 1;
+    }
+    .Entry__place {
+      //margin-left: 0.75rem;
+    }
 
     @include respond-below(sm) {
       display: flex;
@@ -37,9 +44,12 @@ export default {
     }
   }
 
-  &__place {
+  &__title {
     font-size: 1.1em;
-    font-weight: 400;
+  }
+
+  &__place {
+    font-size: 1em;
     @include respond-below(sm) {
       font-size: 1.1em;
     }
@@ -61,6 +71,7 @@ export default {
   }
 
   &__details {
+    font-size: 0.95em;
     padding-left: 0.5em;
     line-height: 1.3;
 
